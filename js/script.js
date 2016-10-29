c26eaf5a765bd6c4bd3701edca5186db0bebd1cd
forgoroe <forgoroe@gmail.com>
2016-10-29 16:03:43 +0200

    add sorting to list of streams

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var sortOrder = 'byViewCount';
 window.onload = function() {
+    var sortOrder = 'byViewCount';
     var CLIENT_ID = 'arni47763m6bx3atc5gnhcpnyfh40m9';
 
     Twitch.init({
@@ -8,11 +8,15 @@ window.onload = function() {
         if (error) {
             return console.log(error);
         }
-        var displayName;
-        var twitchAvatar;
-        sortOrder = 'byViewCount';
-        $('.twitch-connect').hide(100);
-        $('#logout').show(400);
+        requestChannels();
+    });
+
+    function requestChannels(byOrder) {
+        if (!byOrder) {
+            byOrder = sortOrder;
+        }
+        sortOrder = byOrder;
+
         Twitch.api({
             method: 'user',
             params: {
@@ -22,24 +26,23 @@ window.onload = function() {
             if (error) {
                 return console.log(error);
             }
-
-            displayName = data.display_name;
-            twitchAvatar = data.logo;
+            var userInfo = {
+                displayName: data.display_name,
+                twitchAvatar: data.logo,
+            }
             Twitch.api({
                 method: 'streams/followed',
                 params: {
-                    limit: 100,
+                    limit: 50,
                     stream_type: 'live'
                 }
             }, function(error, streams) {
-                organiseData(true, streams, sortOrder, function(dataNeeded) {
-                    displayList(true, displayName, twitchAvatar, dataNeeded);
+                organiseData(true, streams, byOrder, function(dataNeeded) {
+                    displayList(true, userInfo, dataNeeded);
                 });
             });
-          });
-        }
-      );
-
+        });
+    }
     $('.twitch-connect').click(function() {
         Twitch.login({
             scope: ['user_read', 'channel_read']
@@ -48,15 +51,31 @@ window.onload = function() {
 
     $('#logout').click(function() {
         Twitch.logout(function(error) {
-          $('#userInfo').hide(100);
-          $('#orderSection').hide();
-          $('#logout').hide(100);
-          $('.twitch-connect').show(400);
-          $('#listOfChannels').hide(100);
+            $('#userInfo').hide(100);
+            $('#orderSection').hide();
+            $('#logout').hide(100);
+            $('.twitch-connect').show(400);
+            $('#listOfChannels').hide(100);
         });
     });
 
-    function organiseData(authenticated, streams, sortOrder, giveBack) {
+    $('#byViewCount').click(function() {
+        if (sortOrder !== 'byViewCount') {
+            requestChannels('byViewCount');
+            $(this).parent().addClass('active');
+            $('#byName').parent().removeClass('active');
+        }
+    });
+
+    $('#byName').click(function() {
+        if (sortOrder !== 'byName') {
+            requestChannels('byName');
+            $(this).parent().addClass('active');
+            $('#byViewCount').parent().removeClass('active');
+        }
+    });
+
+    function organiseData(authenticated, streams, order, giveBack) {
         if (authenticated && streams) {
             var streamList = streams.streams;
             var dataNeeded = [];
@@ -73,67 +92,66 @@ window.onload = function() {
                 };
             }
 
-          switch(sortOrder){
-            case 'byName':
-            dataNeeded.sort(function(a,b){
-              if(a.name.toUpperCase() > b.name.toUpperCase()){
-                return 1;
-              }
-              if(a.name.toUpperCase() < b.name.toUpperCase()){
-                return -1;
-              }
-              return 0;
-            });
-            break;
+            switch (order) {
+                case 'byName':
+                    dataNeeded.sort(function(a, b) {
+                        if (a.name.toUpperCase() > b.name.toUpperCase()) {
+                            return 1;
+                        }
+                        if (a.name.toUpperCase() < b.name.toUpperCase()) {
+                            return -1;
+                        }
+                        return 0;
+                    });
+                    break;
 
-            case 'byViewCount':
-            dataNeeded.sort(function(a,b){
-              return b.viewers - a.viewers;
-            });
-            break;
-          }
+                case 'byViewCount':
+                    dataNeeded.sort(function(a, b) {
+                        return b.viewers - a.viewers;
+                    });
+                    break;
+            }
         }
         giveBack(dataNeeded);
     }
 
-    function displayList(authenticated, userName, twitchAvatar, dataNeeded){
-      if (authenticated) {
-          $('.twitch-connect').hide(100);
-          $('#user').html(userName);
-          $('#userImage').attr('src', twitchAvatar);
-          $('#following').html(dataNeeded.length);
-          $('#userInfo').show(400);
-          $('#orderSection').show(400);
-          $('#logout').show(400);
-      } else {
-          $('#userInfo').hide(100);
-          $('#orderSection').hide();
-          $('#logout').hide(100);
-          $('.twitch-connect').show(400);
-          $('#listOfChannels').hide(100);
-      }
-      var listOfChannels = document.getElementById('listOfChannels');
-      if (listOfChannels.hasChildNodes()) {
-           $(listOfChannels).hide(400, function() {
-               while (listOfChannels.firstChild) {
-                   listOfChannels.removeChild(listOfChannels.firstChild);
-               }
-           });
-         }
+    function displayList(authenticated, userInfo, dataNeeded) {
+        var listOfChannels = document.getElementById('listOfChannels');
+        $(listOfChannels).children().hide(200, function() {
+            $(this).empty();
+        });
+
+        if (authenticated) {
+            $('.twitch-connect').hide(100);
+            $('#user').html(userInfo.displayName);
+            $('#userImage').attr('src', userInfo.twitchAvatar);
+            $('#following').html(dataNeeded.length);
+            $('#userInfo').show(400);
+            $('#orderSection').show(400);
+            $('#logout').show(400);
+        } else {
+            $('#userInfo').hide(100);
+            $('#orderSection').hide();
+            $('#logout').hide(100);
+            $('.twitch-connect').show(400);
+            $('#listOfChannels').hide(100);
+        }
+
+
         for (var i = 0; i < dataNeeded.length; i++) {
             $('#listOfChannels')
                 .append('<div class="row list-group-item">' +
-                    '<div class="col-md-offset-1 col-md-5 preview">' +
+                    '<div class="col-md-5 preview">' +
                     '<a href="' + dataNeeded[i].url + '"target="_blank" class="list-group-item-heading"><img src="' + dataNeeded[i].preview + '" alt="twitchThumbnail"></a>' +
                     '</div>' +
-                    '<div class="col-md-5 col-md-offset-1 info">' +
+                    '<div class="col-md-5 col-md-offset-2 info">' +
                     '<a href="' + dataNeeded[i].url + '"target="_blank""><h1><img src="' + dataNeeded[i].channelLogo + '" alt="channelLogo">  ' + dataNeeded[i].name + '</h1></a>' +
                     '<p class="list-group-item-text">' + dataNeeded[i].status + '</p>' +
                     '<h3>Playing: ' + dataNeeded[i].game + '</h3>' +
                     '<h3>Viewers: ' + dataNeeded[i].viewers + '</h3>' +
                     '</div>' +
                     '</div>');
-            $('#listOfChannels').show(400);
+            $('#listOfChannels').show(500);
         }
     }
 }
